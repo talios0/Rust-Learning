26ac6d47b5141c0a132f51222e18593b3a08f335
Charles <charlesreverand.01@gmail.com>
2018-06-09 21:55:48 -0400

    Operators overview
    Following an online course

--- Primitive Types/src/main.rs
+++ Primitive Types/src/main.rs
@@ -1,6 +1,11 @@
 use std::mem;
 
 fn main() {
+    primitive_types();
+    operators();
+}
+
+fn primitive_types() {
     //variables are constant by default (immutable)
 
     //unsigned (Positive numbers)
@@ -32,5 +37,32 @@ fn main() {
     println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
     
     let _f = 4>0; // true
+}
 
+fn operators() {
+    // arithmetic
+    let mut a = 2+3*4;
+    println!("{}", a);
+    a +=11; //-- and ++ do not exit in Rust
+    a -=2;
+
+    println!("Remainder of {} / {} = {}", a, 3, a%3); // % is modulo, takes the remainer of a/3
+
+    let a_cubed = i32::pow(a, 3); // a raised to the third power
+    println!("{} cubed is {}", a, a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3); // i represents that b raised to the third will be rounded to an integer
+    let b_to_pi = f64::powf(b,std::f64::consts::PI);
+    println!("{} cubed = {}, {}^pi = {}", b, b_cubed, b, b_to_pi);
+
+    //bitwise operators (integers only)
+    let c = 1 | 2; // | or & and ^and ^ exclusive or (xor) ! not
+    // 0001  or 0010 = 11 = 3 (decimal)
+    println!("1|2 = {}", c)
+    let two_to_10 = 1 << 10;
+    println!("2^10 = {}", two_to_10);
+
+    //logical operators
+    let pi_less_4 = std::f64:consts::PI < 4.0; // true
 }
